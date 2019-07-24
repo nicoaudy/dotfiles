00e6ca4d557f99c7a3bf9e6a2b5c040e7aeb79b0
Nico Audy <nicoaudy1994@gmail.com>
2019-07-24 12:39:58 +0700

    update vim install

--- scripts/vim-install.sh
+++ scripts/vim-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir -p $1/.vim/
-cp -i vim/.vimrc $1/.vimrc
-cp -i vim/plugin.vim $1/.vim/
-cp -i -r vim/snippets $1/.vim/
-cp -i -r vim/bundle $1/.vim/
+mkdir -p $HOME/.vim/
+cp -i ../vim/.vimrc $HOME/.vimrc
+cp -i ../vim/plugins.vim $HOME/.vim/plugins.vim
+cp -i -r ../vim/snippets $HOME/.vim/
+cp -i -r ../vim/bundle $HOME/.vim/
 
 # Get Vundle plugin
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
