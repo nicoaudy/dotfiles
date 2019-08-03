02c8b20b7b9ee3e94df511fb915b97dd85d4c8ff
Nico Audy <nicoaudy1994@gmail.com>
2019-08-03 11:22:33 +0700

    Update and rename vim-install.sh to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,9 @@ cp -i ../vim/plugins.vim $HOME/.vim/plugins.vim
 cp -i -r ../vim/snippets $HOME/.vim/
 cp -i -r ../vim/bundle $HOME/.vim/
 
+# Copy zsh
+cp -i ../.zshrc $HOME/.zshrc
+
 # Get Vundle plugin
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
