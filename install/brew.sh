fa68d487cf1d2661ebc909c4457efa855682c085
Cody Mac <nicoaudy1994@gmail.com>
2020-01-01 12:33:56 +0700

    check the avail brew

--- install/brew.sh
+++ install/brew.sh
@@ -1,6 +1,12 @@
 # Install Homebrew
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 brew tap homebrew/versions
 brew tap homebrew/dupes
 brew update
