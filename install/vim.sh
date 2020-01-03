21436066d5f789cd75baeaa0479842054c105b2d
Nico Audy <nicoaudy1994@gmail.com>
2020-01-03 16:09:36 +0700

    write more snippers

--- install/vim.sh
+++ install/vim.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 mkdir -p $HOME/.vim/
-cp -i "$DOTFILES_DIR/vim/.vimrc" $HOME/.vimrc
-cp -i "$DOTFILES_DIRvim/plugins.vim" $HOME/.vim/plugins.vim
-cp -i -r "$DOTFILES_DIR/vim/snippets" $HOME/.vim/
+#cp -i "$DOTFILES_DIR/vim/.vimrc" $HOME/.vimrc
+#cp -i "$DOTFILES_DIRvim/plugins.vim" $HOME/.vim/plugins.vim
+#cp -i -r "$DOTFILES_DIR/vim/snippets" $HOME/.vim/
+
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" $HOME/.vimrc
+ln -sfv "$DOTFILES_DIR/vim/plugins.vim" $HOME/.vim/plugins.vim
+ln -sfv "$DOTFILES_DIR/vim/snippets" $HOME/.vim/
 
 # Get Vundle plugin
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
