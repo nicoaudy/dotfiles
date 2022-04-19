54324609147930db583dc96a08e06fe149d97ba0
Nico Audy <nicoaudy1994@gmail.com>
2022-04-19 15:33:21 +0700

    ++ treesitter to lua vim

--- nvim_lua/lua/user/plugins.lua
+++ nvim_lua/lua/user/plugins.lua
@@ -85,10 +85,10 @@ return packer.startup(function(use)
   use "nvim-telescope/telescope.nvim"
 
   -- Treesitter
-  -- use {
-    -- "nvim-treesitter/nvim-treesitter",
-    -- run = ":TSUpdate",
-  -- }
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
   -- use "JoosepAlviste/nvim-ts-context-commentstring"
 
   -- Git
