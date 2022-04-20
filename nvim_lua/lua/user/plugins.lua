c7399c909d15a1afd0e573d22825b53a5eb2385b
Nico Audy <nicoaudy1994@gmail.com>
2022-04-20 11:21:00 +0700

    ++ impatient lua and lualine

--- nvim_lua/lua/user/plugins.lua
+++ nvim_lua/lua/user/plugins.lua
@@ -50,10 +50,10 @@ return packer.startup(function(use)
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
-  -- use "nvim-lualine/lualine.nvim"
+  use "nvim-lualine/lualine.nvim"
   use "akinsho/toggleterm.nvim"
   -- use "ahmedkhalf/project.nvim"
-  -- use "lewis6991/impatient.nvim"
+  use "lewis6991/impatient.nvim"
   -- use "lukas-reineke/indent-blankline.nvim"
   -- use "goolord/alpha-nvim"
   -- use "antoinemadec/FixCursorHold.nvim"         -- This is needed to fix lsp doc highlight
