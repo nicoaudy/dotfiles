5c8c4b2d9f368ff813e09b498adbc983dcc24012
Nico Audy <nicoaudy1994@gmail.com>
2022-04-20 11:22:45 +0700

    indentline

--- nvim_lua/lua/user/plugins.lua
+++ nvim_lua/lua/user/plugins.lua
@@ -54,7 +54,7 @@ return packer.startup(function(use)
   use "akinsho/toggleterm.nvim"
   -- use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
-  -- use "lukas-reineke/indent-blankline.nvim"
+  use "lukas-reineke/indent-blankline.nvim"
   -- use "goolord/alpha-nvim"
   -- use "antoinemadec/FixCursorHold.nvim"         -- This is needed to fix lsp doc highlight
   -- use "folke/which-key.nvim"
