df6b27a553629d2f30847d624db9e86fdb713709
Nico Audy <nicoaudy1994@gmail.com>
2022-04-20 11:26:09 +0700

    add alpha start neovim😅

--- nvim_lua/lua/user/plugins.lua
+++ nvim_lua/lua/user/plugins.lua
@@ -55,7 +55,7 @@ return packer.startup(function(use)
   -- use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
   use "lukas-reineke/indent-blankline.nvim"
-  -- use "goolord/alpha-nvim"
+  use "goolord/alpha-nvim"
   -- use "antoinemadec/FixCursorHold.nvim"         -- This is needed to fix lsp doc highlight
   -- use "folke/which-key.nvim"
 
