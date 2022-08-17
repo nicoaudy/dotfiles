a1245e619343e329bc5a8c23fdc71aad76dd258d
NicoAudy <nicoaudy1994@gmail.com>
2022-08-17 19:53:42 +0700

    add nvim 2022 setup

---
+++ nvim-2022/init.lua
@@ -0,0 +1,7 @@
+require('base')
+require('highlights')
+require('maps')
+require('plugins')
+
+vim.opt.clipboard:append { 'unnamedplus' }
+
---
+++ nvim-2022/lua/plugins.lua
@@ -0,0 +1,16 @@
+local status, packer = pcall(require, 'packer')
+if (not status) then
+  print("Packer is not installed")
+  return
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use 'hoob3rt/lualine.nvim' -- Statusline
+end)
