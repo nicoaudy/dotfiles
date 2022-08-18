db2dbdaa6b5b9cb50bd18d7f983b2495a500d511
Nico Audy <nicoaudy1994@gmail.com>
2022-08-18 16:28:37 +0700

    remove the not working code

--- nvim_lua/lua/user/plugins.lua
+++ nvim_lua/lua/user/plugins.lua
@@ -80,7 +80,6 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig"                   -- enable LSP
   use "williamboman/nvim-lsp-installer"         -- simple to use language server installer
-  use "onsails/lspkind-nvim"
   use "jose-elias-alvarez/null-ls.nvim"         -- for formatters and linters
 
   -- Telescope
