e4cc4bc30e42c3fd943f3af42675692aa9dc8190
Nico <your@email.tld>
2025-01-15 21:44:32 +0700

    feat: fix auto save either prettier or biome

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,30 @@
+local function biome_lsp_or_prettier(bufnr)
+  local has_biome_lsp = vim.lsp.get_clients({
+    bufnr = bufnr,
+    name = "biome",
+  })[1]
+  if has_biome_lsp then
+    return {}
+  end
+  local has_prettier = vim.fs.find({
+    -- https://prettier.io/docs/en/configuration.html
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.js",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+    "prettier.config.js",
+    "prettier.config.cjs",
+  }, { upward = true })[1]
+  if has_prettier then
+    return { "prettierd", "prettier" }
+  end
+  return { "biome" }
+end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -6,15 +33,14 @@ return {
         lua = { "stylua" },
         css = { "prettierd", "prettier" },
         html = { "prettierd", "prettier" },
-        javascript = { "prettierd", "prettier" },
-        javascriptreact = { "prettierd", "prettier" },
-        typescript = { "prettierd", "prettier" },
-        typescriptreact = { "prettierd", "prettier" },
+        javascript = biome_lsp_or_prettier,
+        typescript = biome_lsp_or_prettier,
+        javascriptreact = biome_lsp_or_prettier,
+        typescriptreact = biome_lsp_or_prettier,
+        json = biome_lsp_or_prettier,
+        jsonc = biome_lsp_or_prettier,
         python = { "isort", "black" },
         php = { "php-cs-fixer" },
-      },
-      default_format = {
-        timeout_ms = 10000,
       },
     },
   },
